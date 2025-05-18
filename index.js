44915472bcaee367a6522a60606b16372811e86f
Addomike <addomichaelopare@gmail.com>
2025-05-18 21:39:39 +0000

    bug5

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const bodyParser = require("body-parser")
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const authRouter = require('./routers/authRouter');
@@ -16,7 +15,6 @@ const payStackRouter = require("./routers/payStackRouter")
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser())
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 
@@ -90,7 +88,7 @@ app.use('/auth', authRouter);
 app.use('/api', productRouter)
 app.use('/api', subscribeRoute)
 app.use('/api', testimonialRouter)
-app.use('/api', bodyParser.raw({ type: 'application/json' }), payStackRouter)
+app.use('/api', payStackRouter)
 
 
 
